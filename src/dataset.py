ede72989e3f33ce60ed364984585e5b50868fd2c
aik2 <aik2mlj@gmail.com>
2022-06-28 16:40:54 +0400

    [Dataset]

--- src/dataset.py
+++ src/dataset.py
@@ -1,12 +1,17 @@
+# pyright: reportOptionalSubscript=false
+
 import torch
 from torchaudio.transforms import MelScale
 from torch.utils.data import Dataset, DataLoader
-from .data_sample import TrainDataSample
 from .utils import (
     pianotree_pitch_shift,
     chd_pitch_shift,
     chd_to_onehot,
     pr_mat_pitch_shift,
+    nmat_to_pianotree_repr,
+    nmat_to_pr_mat_repr,
+    nmat_to_rhy_array,
+    compute_pr_mat_feat,
 )
 from .amc_dl.torch_plus import DataLoaders
 from .audio_sp_modules.my_collate import default_collate
@@ -15,181 +20,184 @@ from .constants import *
 from .utils import read_split_dict
 
 
-class AudioMidiDataset(Dataset):
-    def __init__(self, data_samples):
+class DataSampleNpz:
+    """
+    A single song segment stored in .npz format
+    contains "notes" and "chord"
 
-        # a list of data_sample.TrainDataSample
+    This class aims to get input samples for a single song
+    `_get_item_by_db` is used for retrieving ready-made inputs to the model
+    """
+
+    def __init__(self, fpath, use_chord) -> None:
+        self.fpath = fpath
+        """
+        notes (onset_beat, onset_bin, duration, pitch, velocity)
+        chord for each beat (root, semitone_bitmap, bass)
+        start_table : i-th row indicates the starting row of the "notes" array at i-th beat.
+        db_pos: an array of downbeat beat_ids
+
+        dict : each downbeat corresponds to a SEG_LGTH-long batch
+        """
+
+        # self.notes = None
+        # self.chord = None
+        # self.start_table = None
+        # self.db_pos = None
+
+        # self._nmat_dict = None
+        # self._pianotree_dict = None
+        # self._pr_mat_dict = None
+        # self._feat_dict = None
+
+        # def load(self, use_chord=False):
+        #     """ load data """
+
+        data = np.load(self.fpath)
+        self.notes = data["notes"]
+        self.chord = data["chord"] if use_chord else None
+        self.start_table = data["start_table"]
+        self.db_pos = data["db_pos"]
+
+        self._nmat_dict = dict(zip(self.db_pos, [None] * len(self.db_pos)))
+        self._pianotree_dict = dict(zip(self.db_pos, [None] * len(self.db_pos)))
+        self._pr_mat_dict = dict(zip(self.db_pos, [None] * len(self.db_pos)))
+        self._feat_dict = dict(zip(self.db_pos, [None] * len(self.db_pos)))
+
+    def __len__(self):
+        """Return number of 8-beat segments in a song"""
+        # NOTE: this is correct because we can feed every 2 bar starting from every downbeat
+        # so there will be overlaps
+        return len(self.db_pos)
+
+    def note_mat_batch_at_db(self, db):
+        """
+        Select rows (notes) of the note_mat which lie between beats
+        [db: db + 8].
+        """
+
+        s_ind = self.start_table[db]
+        e_ind = self.start_table[db + SEG_LGTH]
+        seg_mats = self.notes[s_ind, e_ind]
+        return seg_mats
+
+    @staticmethod
+    def reset_db_to_zeros(note_mat, db):
+        note_mat[:, 0] -= db
+
+    @staticmethod
+    def format_reset_seg_mat(seg_mat):
+        """
+        The input seg_mat is (N, 6)
+        The output seg_mat is (N, 3). Columns for onset, pitch, duration.
+        Onset ranges between range(0, 32).
+        """
+
+        output_mat = np.zeros((len(seg_mat), 3), dtype=np.int64)
+        output_mat[:, 0] = seg_mat[:, 0] * N_BIN + seg_mat[:, 1]
+        output_mat[:, 1] = seg_mat[:, 3]
+        output_mat[:, 2] = seg_mat[:, 2]
+        return output_mat
+
+    def store_nmat_seg(self, db):
+        """
+        Get note matrix (SEG_LGTH) at db position
+        """
+        if self._nmat_dict[db] is not None:
+            return
+
+        nmat = self.note_mat_batch_at_db(db)
+        self.reset_db_to_zeros(nmat, db)
+
+        nmat = self.format_reset_seg_mat(nmat)
+        self._nmat_dict[db] = nmat
+
+    def store_prmat_seg(self, db):
+        """
+        Get piano roll format (SEG_LGTH) from note matrices at db position
+        """
+        if self._pr_mat_dict[db] is not None:
+            return
+
+        prmat = nmat_to_pr_mat_repr(self._nmat_dict[db])
+        self._pr_mat_dict[db] = prmat
+
+    def store_features_seg(self, db):
+        """
+        Get symbolic features (SEG_LGTH) according to A2S
+        """
+        if self._feat_dict[db] is not None:
+            return
+
+        # FIXME: Are these features correct for the whole nmat ?
+        rhy = nmat_to_rhy_array(self._nmat_dict[db])
+        bass_prob, pno_intensity = compute_pr_mat_feat(self._pr_mat_dict[db])
+        self._feat_dict[db] = np.stack([bass_prob, pno_intensity, rhy], -1)
+
+    def store_pno_tree_seg(self, db):
+        """
+        Get PianoTree representation (SEG_LGTH) from nmat
+        """
+        if self._pianotree_dict[db] is not None:
+            return
+
+        self._pianotree_dict[db] = nmat_to_pianotree_repr(self._nmat_dict[db])
+
+    def _store_seg(self, db):
+        self.store_nmat_seg(db)
+        self.store_prmat_seg(db)
+        self.store_features_seg(db)
+        self.store_pno_tree_seg(db)
+
+    def _get_item_by_db(self, db):
+        """
+        Return segments of
+            pianotree, chord, prmat, feature
+        """
+        self._store_seg(db)
+
+        # chord
+        seg_chd = self.chord[db : db + SEG_LGTH]
+        # pianotree
+        seg_pno_tree = self._pianotree_dict[db]
+        # prmat
+        seg_prmat = self._pr_mat_dict[db]
+        # symbolic feature
+        seg_feat = self._feat_dict[db]
+
+        return seg_pno_tree, seg_chd, seg_prmat, seg_feat
+
+
+class PianoOrchDataset(Dataset):
+    def __init__(self, data_samples, use_chord=False):
+        super(PianoOrchDataset, self).__init__()
+
+        # a list of DataSampleNpz
         self.data_samples = data_samples
+        self.use_chord = use_chord
 
-        # a list of complete sample counts in each song
         self.lgths = np.array([len(d) for d in self.data_samples], dtype=np.int64)
-
         self.lgth_cumsum = np.cumsum(self.lgths)
 
     def __len__(self):
         return self.lgth_cumsum[-1]
 
-    def __getitem__(self, item):
+    def __getitem__(self, index):
         # song_no is the smallest id that > dataset_item
-        song_no = np.where(self.lgth_cumsum > item)[0][0]
-        song_item = item - np.insert(self.lgth_cumsum, 0, 0)[song_no]
+        song_no = np.where(self.lgth_cumsum > index)[0][0]
+        song_item = index - np.insert(self.lgth_cumsum, 0, 0)[song_no]
 
-        # fetch segment
         song_data = self.data_samples[song_no]
         return song_data[song_item]
 
     @classmethod
-    def load_with_song_ids(cls, song_ids, **kwargs):
-        """song_ids: a list of int/str"""
-        data_samples = [TrainDataSample(song_id, **kwargs) for song_id in song_ids]
-        return cls(data_samples=data_samples)
+    def load_with_song_paths(cls, song_paths, use_chord=False):
+        data_samples = [DataSampleNpz(song_path, use_chord) for song_path in song_paths]
+        return cls(data_samples, use_chord)
 
     @classmethod
-    def load_with_train_valid_ids(cls, tv_song_ids, **kwargs):
-        """tv_song_ids: a tuple of (train_song_ids, valid_song_ids)"""
-        return cls.load_with_song_ids(tv_song_ids[0], **kwargs), cls.load_with_song_ids(
-            tv_song_ids[1], **kwargs
+    def load_train_and_valid_sets(cls, use_chord=False):
+        split = read_split_dict(None, None)
+        return cls.load_with_song_paths(split[0], use_chord), cls.load_with_song_paths(
+            split[1], use_chord
         )
-
-    @classmethod
-    def load_train_and_valid_sets(cls, meter, n_subdiv, **kwargs):
-        """
-        meter: 2, 3, or None.
-            To select subset of pieces in duple(2) or triple(3) meter.
-            None means select all songs.
-        n_subdiv: 2, 3, or None.
-            To select subset of pieces having 2 or 3-based subdivision.
-            None means all.
-        """
-        return cls.load_with_train_valid_ids(read_split_dict(meter, n_subdiv), **kwargs)
-
-
-def collate_fn(batch, device, augment_p, mel_scale):
-    """
-    The customized collate_fn function used in DataLoader.
-    augment_p: the probability of pitch shift. If None, no pitch shift applied.
-
-    The function
-    1) pitch-shift augment the symbolic data
-    2) batching and converting data device and to cuda possibly.
-    3) pitch-shift augment the audio data.
-    """
-
-    def sample_with_p():
-        return np.random.choice(np.arange(-6, 6), 1, p=augment_p)[0]
-
-    def sample_op(b):
-        # b: (pianotree, chord, audio, piano-roll, symbolic feature)
-        pt, c, a, pm, feat = b
-        pt = pianotree_pitch_shift(pt, aug_shift)
-        c = chd_to_onehot(chd_pitch_shift(c, aug_shift))
-        pm = None if pm is None else pr_mat_pitch_shift(pm, aug_shift)
-        feat = None if feat is None else feat
-        return pt, c, a, pm, feat
-
-    # random shift degree
-    aug_shift = sample_with_p() if augment_p is not None else 0
-
-    # before collate: pitch shifting the symbolic data
-    batch = [sample_op(b) for b in batch]
-
-    # collate: default pytorch collate function
-    pno_tree, chd, audio, pr_mat, feat = default_collate(batch)
-
-    # after collate: to datatype, device, and pitch shift the audio.
-    pno_tree = pno_tree.long().to(device)
-    chd = chd.float().to(device)
-    audio = audio.float().to(device)
-    pr_mat = None if pr_mat is None else pr_mat.float().to(device)
-    feat = None if feat is None else feat.float().to(device)
-
-    # augment audio on possibly cuda
-    audio = pitch_shift_to_spec(
-        audio, TGT_SR, aug_shift, mel_scale, n_fft=N_FFT, hop_length=HOP_LGTH
-    )
-
-    return pno_tree, chd, audio, pr_mat, feat
-
-
-def create_data_loader(dataset, batch_size, shuffle, aug_p, device, num_workers=0):
-    """create a pytorch data loader with customized collate_fn."""
-    mel_scale = MelScale(
-        n_mels=N_MELS,
-        sample_rate=INPUT_SR,
-        f_min=F_MIN,
-        f_max=F_MAX,
-        n_stft=N_FFT // 2 + 1,
-    ).to(device)
-    return DataLoader(
-        dataset,
-        batch_size,
-        shuffle,
-        num_workers=num_workers,
-        collate_fn=lambda b: collate_fn(b, device, aug_p, mel_scale),
-    )
-
-
-class AudioMidiDataLoaders(DataLoaders):
-
-    """Dataloaders containing train and valid dataloaders."""
-
-    def batch_to_inputs(self, batch):
-        return batch
-
-    @classmethod
-    def get_loaders(
-        cls,
-        bs_train,
-        bs_val,
-        train_dataset,
-        val_dataset,
-        shuffle_train=True,
-        shuffle_val=False,
-        aug_p=None,
-        num_workers=0,
-    ):
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        train_loader = create_data_loader(
-            train_dataset, bs_train, shuffle_train, aug_p, device, num_workers
-        )
-        val_loader = create_data_loader(
-            val_dataset, bs_val, shuffle_val, None, device, num_workers
-        )
-        return cls(train_loader, val_loader, bs_train, bs_val, device)
-
-    @property
-    def train_set(self):
-        return self.train_loader.dataset
-
-    @property
-    def val_set(self):
-        return self.val_loader.dataset
-
-
-def create_data_loaders(
-    batch_size,
-    num_workers=0,
-    meter=2,
-    n_subdiv=2,
-    shuffle_train=True,
-    shuffle_val=False,
-    **kwargs
-):
-    """Fast data loaders initialization."""
-
-    train_dataset, valid_dataset = AudioMidiDataset.load_train_and_valid_sets(
-        meter, n_subdiv, **kwargs
-    )
-
-    aug_p = AUG_P / AUG_P.sum()
-
-    return AudioMidiDataLoaders.get_loaders(
-        batch_size,
-        batch_size,
-        train_dataset,
-        valid_dataset,
-        shuffle_train,
-        shuffle_val,
-        aug_p,
-        num_workers,
-    )
